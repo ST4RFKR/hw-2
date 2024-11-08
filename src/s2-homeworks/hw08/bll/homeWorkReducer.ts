99d1500c309a1c418f1ac38568fcc52c7f94142e
Evg_S <nikelodiym1210@gmail.com>
2024-11-08 13:29:16 +0200

    hw3|3-4-2

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,8 +7,12 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
   switch (action.type) {
     case 'sort': {
       // by name
-      const filteredUp = state.sort((a: UserType, b: UserType) => a.name.localeCompare(b.name));
-      const filteredDonw = state.sort((a: UserType, b: UserType) => b.name.localeCompare(a.name));
+      const filteredUp = [...state].sort((a: UserType, b: UserType) =>
+        a.name.localeCompare(b.name),
+      );
+      const filteredDonw = [...state].sort((a: UserType, b: UserType) =>
+        b.name.localeCompare(a.name),
+      );
 
       return action.payload === 'down' ? filteredUp : filteredDonw;
     }
