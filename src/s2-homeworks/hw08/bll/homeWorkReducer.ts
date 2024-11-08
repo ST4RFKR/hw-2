be2118ce9874d6f249863ea63ccea4d9c3ed5c0f
Evg_S <nikelodiym1210@gmail.com>
2024-11-08 13:17:21 +0200

    hw3|3-4

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,21 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8';
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case 'sort': {
+      // by name
+      const filteredUp = state.sort((a: UserType, b: UserType) => a.name.localeCompare(b.name));
+      const filteredDonw = state.sort((a: UserType, b: UserType) => b.name.localeCompare(a.name));
 
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+      return action.payload === 'up' ? filteredUp : filteredDonw;
     }
-}
+    case 'check': {
+      return state.filter((el: UserType) => el.age >= 18);
+    }
+    default:
+      return state;
+  }
+};
