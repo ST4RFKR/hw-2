3718510c9425ad6ac293966a6d97c786ee97e107
Evg_S <nikelodiym1210@gmail.com>
2024-11-24 15:07:04 +0200

    4/2-10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,30 @@
 const initState = {
-    isLoading: false,
-}
+  isLoading: false,
+};
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
+export const loadingReducer = (state = initState, action: ActionType): { isLoading: boolean } => {
+  // fix any
+  switch (action.type) {
+    // пишет студент  // need to fix
+    case 'CHANGE_LOADING': {
+      return {
+        ...state,
+        isLoading: action.isLoading,
+      };
     }
-}
+    default:
+      return state;
+  }
+};
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+  type: 'CHANGE_LOADING';
+  isLoading: boolean;
+};
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+  type: 'CHANGE_LOADING' as const,
+  isLoading,
+});
+
+type ActionType = ReturnType<typeof loadingAC>;
