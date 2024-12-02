b11072c5b9dd9287b0f8a99d9778e0a033c01483
Eugene <admin@MacBook-Pro-Evgenij.local>
2024-12-02 16:06:49 +0200

    4-4

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,27 @@
 const initState = {
-    themeId: 1,
-}
+  themeId: 1,
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
-
-        default:
-            return state
+export const themeReducer = (state = initState, action: ThemeAction): ThemeState => {
+  // fix any
+  switch (action.type) {
+    case 'SET_THEME_ID': {
+      return {
+        ...state,
+        themeId: action.id,
+      };
     }
-}
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+    default:
+      return state;
+  }
+};
+type ThemeAction = {
+  type: 'SET_THEME_ID';
+  id: number;
+};
+
+export type ThemeState = {
+  themeId: number;
+};
+export const changeThemeId = (id: number): ThemeAction => ({ type: 'SET_THEME_ID', id }); // fix any
