fc157fd5b2cadf5000bca9336923b338c6278dc3
Evg_S <nikelodiym1210@gmail.com>
2024-12-10 00:21:52 +0200

    5-2

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -35,8 +35,8 @@ const HW13 = () => {
       .then((res) => {
         setCode('Код 200!');
         setImage(success200);
-        setText(res.data.message || 'Запрос успешен');
-        setInfo('Запрос выполнен успешно');
+        setText(res.data.message || '...всё ок)');
+        setInfo('код 200 - обычно означает что скорее всего всё ок)');
       })
       .catch((e) => {
         if (e.response) {
@@ -44,19 +44,30 @@ const HW13 = () => {
           if (status === 400) {
             setCode('Код 400!');
             setImage(error400);
-            setText(e.response.data.message || 'Ошибка запроса');
-            setInfo('Вы отправили неправильные данные');
+            setText(e.response.data.message || 'Ты не отправил success в body вообще!');
+            setInfo(
+              'ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!',
+            );
           } else if (status === 500) {
             setCode('Код 500!');
             setImage(error500);
-            setText(e.response.data.message || 'Ошибка на сервере');
-            setInfo('Сервер сломался');
+            setText(e.response.data.message || 'эмитация ошибки на сервере');
+            setInfo(
+              'ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)',
+            );
           }
-        } else {
-          setCode('Error');
+        } else if (e.request) {
+          // Если запрос был отправлен, но сервер не ответил
+          setCode('Error!');
           setImage(errorUnknown);
-          setText(e.message || 'Неизвестная ошибка');
-          setInfo('Ошибка подключения');
+          setText('Network Error');
+          setInfo('AxiosError');
+        } else {
+          // Любая другая ошибка
+          setCode('Error!');
+          setImage(errorUnknown);
+          setText(e.message || 'Unknown Error');
+          setInfo('Ошибка: не удалось отправить запрос');
         }
       })
       .finally(() => {
